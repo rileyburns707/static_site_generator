c4ffec6c4e4798c48469501d1586a21279139f2d
rileyburns707 <rileyburns707@gmail.com>
2026-07-03 12:58:20 -0400

    added unit test setup

---
+++ src/test_testnode.py
@@ -0,0 +1,27 @@
+import unittest
+from textnode import TextNode, TextType
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_different_property(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.TEXT)
+        self.assertNotEqual(node, node2)
+    
+    def test_noteq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a different text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_no_url(self):
+        node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev/lessons/0abc7ce4-3855-4624-9f2d-7e566690fee1")
+        node2 = TextNode("This is a text node", TextType.TEXT)
+        self.assertNotEqual(node, node2)
+
+
+if __name__ == "__main__":
+    unittest.main()
